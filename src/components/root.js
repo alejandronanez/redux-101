a1ed9a3fd68e1b4fceea340c0dfe2f9f10bd6679
Alejandro Nanez <alejonanez@gmail.com>
2016-02-03 15:37:08 -0500

    Root components

---
+++ src/components/root.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Root extends Component {
+    render() {
+        return (
+            <h1>Hi there from Root Component</h1>
+        );
+    }
+}
